355e6c152c628843656c6e73037aa7c040ac0f2f
Jacob Bates <jopb1184@gmail.com>
2021-11-05 10:54:52 -0700

    Java Game

---
+++ Main.java
@@ -0,0 +1,159 @@
+import java.util.Scanner;
+
+class Main {
+    static boolean[][] matrix = {
+        {false, false, false}, 
+        {false, true, false}, 
+        {false, false, false}
+    };
+
+    static int[] sticky = {4, -1};
+    static int[] score = new int[2];
+
+    static Scanner input = new Scanner(System.in);
+    
+    static String prompt = "Col-Row to flip: ";
+    static String exInvalid = "Invalid Chip ID";
+    static String exStuck = "Chip is Stuck";
+
+    static String matrixHead       = " ┌───┬───┬───┬───┐ ";
+    static String matrixDivider    = " ├───┼───┼───┼───┤ ";
+    static String matrixFoot       = " └───┴───┴───┴───┘ ";
+    static String matrixSeparator  = " │ ";
+
+    public static void main(String[] args) {
+        System.out.println("Henlo, worlnd");
+
+        do {
+            printMatrix();
+            score();
+            System.out.println(score[0] + "," + score[1]);
+
+            do {
+                try {
+                    turn();
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                    continue;
+                }
+                break;
+            } while (true);
+        } while (gameExists());
+
+        printMatrix();
+        score();
+        System.out.println(
+            "Player " + 
+            (score[0] > score[1] ? "A" : "B") + 
+            " Wins " + 
+            score[0] + "," + score[1]
+        );
+    }
+
+    // Run one turn
+    static void turn() throws Exception {
+        System.out.print(prompt);
+        int chip = decodeChipID(input.nextLine());
+        flip(chip);
+    }
+
+    // Flip a Particular Chip
+    static void flip(int chip) throws Exception {
+        if (chip == sticky[0] || chip == sticky[1]) throw new Exception(exStuck);
+        sticky[1] = sticky[0];
+        sticky[0] = chip;
+
+        matrix [chip / 3] [chip % 3] = 
+        ! matrix [chip / 3] [chip % 3];
+    }
+
+    // Analyse Scores
+    static void score() {
+        boolean p = matrix[0][0];
+
+        score[0] = 0;
+        for (boolean[] row : matrix) {
+            for (boolean chip : row) {
+                if (chip != p) score[0]++;      // Add a point for each change
+                p = chip;                       // Keep track of last state
+            }
+        }
+
+        p = matrix[2][0];
+
+        score[1] = 0;
+        for (int col = 0; col < 3; col++) {
+            for (int row = 2; row > -1; row--) {
+                if (matrix[row][col] != p) score[1]++;
+                p = matrix[row][col];
+            }
+        }
+    }
+
+    // Check if the game is over
+    static boolean gameExists() {
+        return !(
+            ((matrix[1][1] == matrix[0][0]) && (matrix[1][1] == matrix[2][2])) ||
+            ((matrix[1][1] == matrix[0][2]) && (matrix[1][1] == matrix[2][0]))
+        );
+    }
+
+    // Convert a Chip ID (C2) to an integer (7)
+    static int decodeChipID(String chip) throws Exception {
+        int index = 0;
+
+        if (
+            chip.length() != 2 ||                   // Input is not 2 chars long
+            (int) chip.charAt(1) - '1' > 2          // Digit is not valid
+        ) throw new Exception(exInvalid);
+
+        index = 3 * (int) (chip.charAt(1) - '1');   // Interpret row, row has 3 chips
+
+        switch (chip.charAt(0)) {                   // Interpret column
+            case 'C':
+                index++;
+            case 'B':
+                index++;
+            case 'A':
+                break;
+
+            default:
+                throw new Exception(exInvalid);
+        }
+
+        return index;
+    }
+
+    // Print the Entire Matrix
+    static void printMatrix() {
+        char[] headerRow = {'A', 'B', 'C'};
+
+        System.out.println(matrixHead);
+        printRow(' ', headerRow);
+
+        for (int row = 0; row < 3; row++) {
+            System.out.println(matrixDivider);
+            
+            char[] rowSym = new char[3];
+            
+            for (int chip = 0; chip < 3; chip++) {
+                rowSym[chip] = matrix[row][chip] ? 'X' : 'O';
+            }
+
+            printRow((char) (row + 1 + '0'), rowSym);
+        }
+
+        System.out.println(matrixFoot);
+    }
+
+    // Print a Matrix Row
+    static void printRow(char label, char[] row) {
+        System.out.print(matrixSeparator + label);
+
+        for (char i : row) {
+            System.out.print(matrixSeparator + i);
+        }
+
+        System.out.println(matrixSeparator);
+    }
+}
---
+++ TODO.md
@@ -0,0 +1,8 @@
+# To-Do
+
+- Prompt individual players
+- Display chip orders for each player
+- Make chip states bold
+- Make sticky chips red
+- Keep matrix in a static position
+- Colour changes in chip orders
